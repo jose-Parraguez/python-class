73d2a4d3b2752ba01fb16680f8eb2e0cf5a71e19
jose-parraguez <joseadanparraguez@gmail.com>
2023-03-30 20:46:15 -0300

    add more script

---
+++ control-flujo/02-if.py
@@ -0,0 +1,11 @@
+# Super importante el orden de las funciones
+edad = 15
+
+if edad > 70:
+    print("Puedes ver la pelicula con descuento")
+
+elif edad > 17:
+    print("Puede ver la pelicula")
+
+else:
+    print("no puede ver la pelicula")
---
+++ tipos/09-conversion-tipos.py
@@ -0,0 +1,3 @@
+x = int(3)
+y = str("hola")
+z = float(0.1)
