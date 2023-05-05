cb391c9913f6187b9120db0aa5b505de25a8834d
jose-parraguez <joseadanparraguez@gmail.com>
2023-05-05 09:04:35 -0400

    add: add diferert scrip of python

---
+++ funciones/04-return.py
@@ -0,0 +1,12 @@
+def suma(a, b):
+    resultado = a + b
+    return resultado
+
+
+# se retorna el resultado y se guarda en la variable C
+c = suma(1, 2)
+
+# entonces despues C se puede usar en otra def o para otra operacion
+d = suma(c, 2)
+
+print(c)
---
+++ funciones/05-alcance.py
@@ -0,0 +1,15 @@
+def saludar():
+    saludo = "Hola Mundo"
+    print(saludo)
+
+
+def saludo2():
+    saludo = "hola chanchito"
+    print(saludo)
+
+
+saludar()
+saludo2()
+saludar()
+
+# saludo es diferente en cada una de las funciones
