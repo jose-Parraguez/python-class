0a6843575a1e64d74ce8498d2d1f2f02b0cf69ec
jose-Parraguez <joseadanparraguez@gmail.com>
2023-05-24 11:17:10 -0400

    listas

---
+++ tipos_Avanzados/01-listas.py
@@ -0,0 +1,18 @@
+numero = [1,2,3,5]
+numero2 = [1,2,3,5] * 10
+
+
+#print(numero[0])
+
+cont = 0
+for x in numero:
+    cont += 1
+
+#print(cont)
+#print(numero2)
+
+
+rango = list(range(20))
+palabra = list("Hola mundo")
+print(rango)
+print(palabra)
